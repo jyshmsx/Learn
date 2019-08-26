492d3d20adff3a5ebd5e479878d8fe160eabca62
jyshmsx <jysh_y@163.com>
2019-08-26 19:51:48 +0800

    Update Word.java

--- java/Word.java
+++ java/Word.java
@@ -1,34 +1,15 @@
 import java.util.Scanner;
 import java.util.Arrays;
-import java.util.Comparator;
 public class Word{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
         String[] str = s.split(" ");
-        Comparator cmp = new MyComparator();
-        Arrays.sort(str, cmp);
+        Arrays.sort(str, String.CASE_INSENSITIVE_ORDER);
         for(String name: str)
         {
             System.out.println(name);
         }
         scan.close();
-    }
-}
-
-class MyComparator implements Comparator<String>{
-    public int compare(String o1, String o2) {
-        if(Character.toUpperCase(o1.charAt(0)) > Character.toUpperCase(o2.charAt(0)))
-        {
-            return 1;
-        }
-        else if(Character.toUpperCase(o1.charAt(0)) < Character.toUpperCase(o2.charAt(0)))
-        {
-            return -1;
-        }
-        else
-        {
-            return 0;
-        }
     }
 }
