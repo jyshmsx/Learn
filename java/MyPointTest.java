08e5ab2a433804e114d22b9080742a0e46d5269d
jyshmsx <jysh_y@163.com>
2019-09-04 20:01:21 +0800

    Create ArrayInClass.java

--- java/MyPointTest.java
+++
@@ -1,9 +0,0 @@
-public class MyPointTest{
-    public static void main(String[] args) {
-        MyPoint a = new MyPoint();
-        MyPoint b = new MyPoint();
-        b.set(10,30.5);
-        System.out.println(a.distance(b));
-        System.out.println(b.distance(0.0d, 0.0d));
-    }
-}
