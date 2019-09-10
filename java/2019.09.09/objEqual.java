f2de3bfb9e6b4be704c9c157934b73ba0777a470
jyshmsx <jysh_y@163.com>
2019-09-10 22:18:49 +0800

    Update objEqual.java

--- java/2019.09.09/objEqual.java
+++ java/2019.09.09/objEqual.java
@@ -1,12 +1,17 @@
 package inclass.task.third;
 
+import java.util.Scanner;
+
 public class objEqual{
     public static void main(String[] args) {
         //Student类equals方法
-        Student s1 = new Student(1, "a", 20);
+        Scanner sc = new Scanner(System.in);
+        Student s1 = new Student(sc.nextInt(), sc.next(), sc.nextInt());
         Student s2 = new Student(1, "a", 20);
         System.out.println(s2.equals(s1));
-        // Student cloneS = (Student) a.clone();
-        // System.out.println("clone: " + cloneS.equals(a));
+
+        Student a = new Student(1 , "a", 20);
+        System.out.println("clone: " + a.clone().equals(a));
+        sc.close();
     }
 }
