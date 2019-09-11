d0f229ec1e96010c32221d9a914e37288eeebb0e
jyshmsx <jysh_y@163.com>
2019-09-11 08:31:19 +0800

    Update objEqual.java

--- java/2019.09.09/objEqual.java
+++ java/2019.09.09/objEqual.java
@@ -9,7 +9,8 @@ public class objEqual{
         Student s1 = new Student(sc.nextInt(), sc.next(), sc.nextInt());
         Student s2 = new Student(1, "a", 20);
         System.out.println(s2.equals(s1));
-
+		
+		//clone()方法
         Student a = new Student(1 , "a", 20);
         System.out.println("clone: " + a.clone().equals(a));
         sc.close();
