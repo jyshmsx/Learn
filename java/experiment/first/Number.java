eb8e02773180799d440a369b230ab74515638922
jyshmsx <jysh_y@163.com>
2019-10-10 18:29:49 +0800

    Update Number.java

--- java/experiment/first/Number.java
+++ java/experiment/first/Number.java
@@ -1,6 +1,6 @@
 package inclass.experiment.first;
 
-
+//定义Number类，用以存储数字
 public class Number{
     protected int [] nu;
 
@@ -11,15 +11,24 @@ public class Number{
 
     public void setNumber()
     {
-        System.out.println("请输入一个四位数");
         int n;
-        n = BullsAndCows.sc.nextInt();
+        do
+        {
+            System.out.println("请输入一个四位数");
+            n = BullsAndCows.sc.nextInt();
+        }while(!checkNuber(n));
+        
         for(int i = 0; i < 4; i++)
         {
             int wh =(int)(Math.pow((double)10, (double)(3 - i)));
             nu[i] = n / wh;
             n -= nu[i] * wh;
         }
+    }
+
+    private boolean checkNuber(int n)
+    {
+        return (n > 1000 & n < 10000);
     }
 
     public void showNumber()
