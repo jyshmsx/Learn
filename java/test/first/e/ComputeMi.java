3cb872157a9e678b2b79a2fe5ae973baff5d4f4c
jyshmsx <jysh_y@163.com>
2019-11-06 09:47:28 +0800

    Create ComputeMi.java

---
+++ java/test/first/e/ComputeMi.java
@@ -0,0 +1,12 @@
+package inclass.test.first.e;
+
+/**
+ * ComputeMi
+ */
+public class ComputeMi implements Compute{
+    public int computer(int n, int m)
+    {
+        return n - m;
+    }
+    
+}
