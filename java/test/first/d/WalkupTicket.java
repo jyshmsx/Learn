68dabe834faa5e7bf52e103ba22bdc71cb315a7c
jyshmsx <jysh_y@163.com>
2019-11-06 09:47:01 +0800

    Create WalkupTicket.java

---
+++ java/test/first/d/WalkupTicket.java
@@ -0,0 +1,10 @@
+package inclass.test.first.d;
+
+public class WalkupTicket extends Ticket{
+    public WalkupTicket(String ticketID)
+    {
+        super(ticketID);
+        this.ticketPrice = 50;
+    }
+    
+}
