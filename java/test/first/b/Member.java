c8d1a06a73481dc8f72358f1705093192fb5b65e
jyshmsx <jysh_y@163.com>
2019-11-06 09:47:12 +0800

    Create Member.java

---
+++ java/test/first/b/Member.java
@@ -0,0 +1,15 @@
+package inclass.test.first.b;
+
+class Member {
+    String userName;
+    String userPasw;
+    Member(String userName, String userPasw)
+    {
+        this.userName = userName;
+        this.userPasw = userPasw;
+    }
+    Member()
+    {
+        
+    }
+}
