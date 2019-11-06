8fc6bdb9a314a27742003695413cde409c3015b6
jyshmsx <jysh_y@163.com>
2019-11-06 09:47:21 +0800

    Create UseCompute.java

---
+++ java/test/first/e/UseCompute.java
@@ -0,0 +1,11 @@
+package inclass.test.first.e;
+
+/**
+ * UseCompute
+ */
+public class UseCompute {
+    public void useCom(Compute com, int one, int two)
+    {
+        System.out.println(com.computer(one, two));
+    }
+}
