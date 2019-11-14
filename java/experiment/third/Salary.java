f1ac17243d420b6e3f7ceb83b0f7c4e1e2d91e7d
jyshmsx <jysh_y@163.com>
2019-11-14 16:30:07 +0800

    Create Salary.java

---
+++ java/experiment/third/Salary.java
@@ -0,0 +1,9 @@
+package inclass.experiment.third;
+
+public interface Salary {
+    public void setDaySalary(int salary);
+    public int getDaySalary();
+    public int getMonthSalary();
+    public void checkIN(); 
+    public void settlementSalary();
+}
